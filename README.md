c003210ccca9e8a06e41c11a4af7ea0715b095fa
Rudra Saxena <saxenarudrar@gmail.com>
2021-08-18 20:36:43 +0530

    Add files via upload

--- README.md
+++ README.md
@@ -1,2 +1,84 @@
-# Simulating-Self-Driving-car-safety-system-using-Deep-Learning
-Research Methods project which implements end to end learning solutions on Simulated self-driving environment
+
+# Simulating Self-Driving car and safety system using Deep Learning
+
+Simulating the operation of Self-Driving driving car on virtual enviroment with Convolutional Neural Networks(CNN).
+
+To implement this project I will train, validate and test a model using Keras API. 
+
+Input will be camera data from the dataset, and output will be steering angle to autonomous vehicle.
+
+## Architecture of project:-
+1) Using simuator to generate dataset with human driving behaviour(errorfree).
+2) Design and train a deep Learning model that predicts steering angle with help of raw data.
+3) Train and vaidate the model using 80-20 validation set.
+4) Complete a lap on simulator track autonomously.
+
+#Dependencies and Libraries used.
+
+I have used anaconda to generate enviroment with exactly neeeded Dependencies.
+Make sure you have anaconda installed in order to setup the enviroment
+
+```bash
+  # I recommend using TensorFlow with GPU due to heavy processing of project
+
+    conda env create -f environment-gpu.yml
+```
+
+Libraries:- 
+
+1) Keras:- This helps dealing with image data using TensorFlow2. Having fast experimentation methods helping to make iterations.
+
+Keras allows aids in using GPU tensor operations which will be necessary to train models on. This scaled models can be trained with multiple layers
+
+2) Matplotlib :- Plotting library to generate graph comparisions, used for comparing training and testing dataset accuray.
+
+3) Numpy :- Important library for mathematical operations and matrices. This library is critical to Keras for performing expressions.
+
+4) Pandas :- Data manipulation and analysis tool for data structure operations.
+
+5) scikit-learn :-  Allows performing classification, regression and clustering operations.
+
+
+## Simulator and Requirements
+
+Installing Udacity open-source Simulator is necessary component of this project for both data generation and displaying model abilities.
+
+Setup process and dependencies for Simulator:- 
+1)	Making sure all setup files are present in local directory, they are hosted on GitHub.
+2)	Installing game development engine called Unity which is available for free at host [website](https://unity.com/), it is open source licensed software for use.
+3)	Simulator binary file Version 2 is available for [windows](https://s3-us-west-1.amazonaws.com/udacity-selfdrivingcar/Term1-Sim/term1-simulator-windows.zip)/[mac](https://s3-us-west-1.amazonaws.com/udacity-selfdrivingcar/Term1-Sim/term1-simulator-mac.zip)/[linux](https://s3-us-west-1.amazonaws.com/udacity-selfdrivingcar/Term1-Sim/term1-simulator-linux.zip) platforms, it can be ran after installing unity engine and dependencies. 
+4)	Large storage drive will be required as three camera input and other data can hold up large space if one full run is performed. 
+
+
+This project is tested on following Minimum Specification Requirements:-
+* Intel i5 dual core processor.
+* Minimum 8GB RAM.
+* GPU (Nvidia Preferable)
+* Atleast 10 GB of free storage.
+
+Run the simulator in training mode and record the runs for 5 iterations and copy recorded data in local directory.
+
+Simulator outputs code in .CSV formats and all image data is stored locally.
+
+## Training model
+  Once Simulator is installed just run the binary file and run following command in bash terminal.
+
+``` python model.py ``` 
+
+This will generate a file model-<epoch>.h5
+
+## Running the simulated model
+
+
+I have also uploaded pretrained model called model.h5 to run using that enter following command:-
+
+``` python drive.py model.h5  ``` 
+
+This will automatically start driving the vehicle which is in automatic mode in simulator.
+
+## Author
+[Rudra Saxena](https://github.com/saxenarudra): Myself Rudra Saxena I am Grad Student at Lakehead University.
+
+## Acknowledgement
+Thank you [Dr. Trevor Tomesh](https://github.com/trevortomesh) for help through the project.
+
---
+++ utils.py
@@ -0,0 +1,140 @@
+import cv2, os
+import numpy as np
+import matplotlib.image as mpimg
+
+
+IMAGE_HEIGHT, IMAGE_WIDTH, IMAGE_CHANNELS = 66, 200, 3
+INPUT_SHAPE = (IMAGE_HEIGHT, IMAGE_WIDTH, IMAGE_CHANNELS)
+
+
+def load_image(data_dir, image_file):
+    """
+    Load RGB images from a file
+    """
+    return mpimg.imread(os.path.join(data_dir, image_file.strip()))
+
+
+def crop(image):
+    """
+    Croping the images removing the sky at the top and the car front at the bottom
+    """
+    return image[60:-25, :, :] 
+
+
+def resize(image):
+  
+    #Resize the image to the input shape used by the network model
+    
+    return cv2.resize(image, (IMAGE_WIDTH, IMAGE_HEIGHT), cv2.INTER_AREA)
+
+
+def rgb2yuv(image):
+    
+    #Convert the image from RGB to YUV following the NVIDIA model Parameter
+  
+    return cv2.cvtColor(image, cv2.COLOR_RGB2YUV)
+
+
+def preprocess(image):
+    image = crop(image)
+    image = resize(image)
+    image = rgb2yuv(image)
+    return image
+
+
+def choose_image(data_dir, center, left, right, steering_angle):
+    
+    #Randomly choose an image from the center, left or right, and adjust
+    #the steering angle.
+   
+    choice = np.random.choice(3)
+    if choice == 0:
+        return load_image(data_dir, left), steering_angle + 0.2
+    elif choice == 1:
+        return load_image(data_dir, right), steering_angle - 0.2
+    return load_image(data_dir, center), steering_angle
+
+
+def random_flip(image, steering_angle):
+   
+    if np.random.rand() < 0.5:
+        image = cv2.flip(image, 1)
+        steering_angle = -steering_angle
+    return image, steering_angle
+
+
+def random_translate(image, steering_angle, range_x, range_y):
+  
+    trans_x = range_x * (np.random.rand() - 0.5)
+    trans_y = range_y * (np.random.rand() - 0.5)
+    steering_angle += trans_x * 0.002
+    trans_m = np.float32([[1, 0, trans_x], [0, 1, trans_y]])
+    height, width = image.shape[:2]
+    image = cv2.warpAffine(image, trans_m, (width, height))
+    return image, steering_angle
+
+
+def random_shadow(image):
+    """
+    Generates and adds random shadow
+    """
+    # (x1, y1) and (x2, y2) forms a line
+    # xm, ym gives all the locations of the image
+    x1, y1 = IMAGE_WIDTH * np.random.rand(), 0
+    x2, y2 = IMAGE_WIDTH * np.random.rand(), IMAGE_HEIGHT
+    xm, ym = np.mgrid[0:IMAGE_HEIGHT, 0:IMAGE_WIDTH]
+
+    mask = np.zeros_like(image[:, :, 1])
+    mask[(ym - y1) * (x2 - x1) - (y2 - y1) * (xm - x1) > 0] = 1
+
+    # choose which side should have shadow 
+    cond = mask == np.random.randint(2)
+    s_ratio = np.random.uniform(low=0.2, high=0.5)
+
+    hls = cv2.cvtColor(image, cv2.COLOR_RGB2HLS)
+    hls[:, :, 1][cond] = hls[:, :, 1][cond] * s_ratio
+    return cv2.cvtColor(hls, cv2.COLOR_HLS2RGB)
+
+
+def random_brightness(image):
+
+    hsv = cv2.cvtColor(image, cv2.COLOR_RGB2HSV)
+    ratio = 1.0 + 0.4 * (np.random.rand() - 0.5)
+    hsv[:,:,2] =  hsv[:,:,2] * ratio
+    return cv2.cvtColor(hsv, cv2.COLOR_HSV2RGB)
+
+
+def augument(data_dir, center, left, right, steering_angle, range_x=100, range_y=10):
+  
+    image, steering_angle = choose_image(data_dir, center, left, right, steering_angle)
+    image, steering_angle = random_flip(image, steering_angle)
+    image, steering_angle = random_translate(image, steering_angle, range_x, range_y)
+    image = random_shadow(image)
+    image = random_brightness(image)
+    return image, steering_angle
+
+
+def batch_generator(data_dir, image_paths, steering_angles, batch_size, is_training):
+    
+    #Generate training image give image paths and associated steering angles
+    
+    images = np.empty([batch_size, IMAGE_HEIGHT, IMAGE_WIDTH, IMAGE_CHANNELS])
+    steers = np.empty(batch_size)
+    while True:
+        i = 0
+        for index in np.random.permutation(image_paths.shape[0]):
+            center, left, right = image_paths[index]
+            steering_angle = steering_angles[index]
+            # argumentation
+            if is_training and np.random.rand() < 0.6:
+                image, steering_angle = augument(data_dir, center, left, right, steering_angle)
+            else:
+                image = load_image(data_dir, center) 
+            # add the image and steering angle to the batch
+            images[i] = preprocess(image)
+            steers[i] = steering_angle
+            i += 1
+            if i == batch_size:
+                break
+        yield images, steers
+
